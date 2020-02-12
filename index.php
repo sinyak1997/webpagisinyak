851be1a9cb435101f3607c47245a05f9aa1bb0f3
sinyak1997 <sibacott.com@gmail.com>
2020-02-12 12:07:09 +0700

    update lagi cuii

--- index.php
+++ index.php
@@ -21,15 +21,216 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
 </head>
 <body>
-    
+            <!--================================== open navbar ==============================-->
+    <nav class="navbar navbar-expand-lg navbar-dark " style="background-image: linear-gradient(45deg, #1ca296, #a947d2);">
+        <a class="navbar-brand" href="#"><img src="html/img/tes.png" width=120px alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </div>
+    </nav>
 
+    <!--======================================= close navbar ==============================-->
 
+    <!--=========================== open container ======================================-->
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            <!-- contennya -->
+                <h1>Hallo</h1>
+            </div>
+            <div class="col-md-3">
+            <!-- contennya -->
+                <h1>Halli</h1>
+            </div>
+            <div class="col-md-3">
+            <!-- contennya -->
+                <h1>Hallu</h1>
+            </div>
+            <div class="col-md-3">
+            <!-- contennya -->
+                <h1>Halla</h1>
+            </div>
+        </div>
+    </div>
 
-    
+        <!--=========================== close container ======================================-->
+
+            <!--============================= open slider ====================================-->
+
+<div class="container-sm">
+    <div class="row">
+        <div class=col-md-1>
+        </div>
+
+    <!-- pengisian html slider -->
+    <div class=col-md-10>
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="html/img/gambar1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="html/img/gambar2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="html/img/gambar4.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
+                </div>
+                </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+        </div>
+    </div>
+    <div class=col-md-1>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+
+                    <!--============================= close slider ====================================-->
+
+                <!--==================================== footer =======================================-->
+
+<footer class="page-footer font-small navbar-dark bg-" style="background-image: linear-gradient(45deg, #1ca296, #a947d2); pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+        <div class="row">
+
+    <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+            <h5 class="text-uppercase">Footer Content</h5>
+            <p>Here you can use rows and columns to organize your footer content.</p>
+
+        </div>
+    <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+
+    <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+            <li>
+                <a href="#!">Link 1</a>
+            </li>
+            <li>
+                <a href="#!">Link 2</a>
+            </li>
+            <li>
+                <a href="#!">Link 3</a>
+            </li>
+            <li>
+                <a href="#!">Link 4</a>
+            </li>
+        </ul>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+            <li>
+                <a href="#!">Link 1</a>
+            </li>
+            <li>
+                <a href="#!">Link 2</a>
+            </li>
+            <li>
+                <a href="#!">Link 3</a>
+            </li>
+            <li>
+                <a href="#!">Link 4</a>
+            </li>
+        </ul>
+
+    </div>
+        <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/"> Sinyak</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+
+                <!--==================================== footer =======================================-->
+
 
     <!-- js for bundle -->
+    <script src="assets/js/jquary.js"></script>
     <script src="assets/js/bootstrap.bundle.js"></script>
     <script src="assets/js/bootstrap.bundle.js.map"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
